7cd4a097d2494811e17d6b1da2b63a66b4a2a31f
KrisssBrown <cristianbruno.cs@gmail.com>
2023-04-09 19:09:13 +0200

    ADD home & contacts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        .container {
+            margin: 0 auto;
+            padding: 200px 0;
+            box-sizing: border-box;
+            max-width: 700px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Questa è la homepage!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia incidunt dicta eaque doloribus, dolorem iste perferendis, nam ex vitae mollitia nulla dolores ipsam perspiciatis! Maiores quas reprehenderit asperiores repellat recusandae ipsa, deleniti animi, delectus, voluptates sit cumque enim vitae! Quod culpa a sunt temporibus. Modi laudantium totam incidunt amet laborum sint, eligendi natus delectus eos libero minima nulla blanditiis nam quaerat repudiandae cum tempore distinctio! Perspiciatis sunt eius reprehenderit molestias corporis, nisi corrupti! Ea, magni error optio consequatur sint est!</p>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+
+
